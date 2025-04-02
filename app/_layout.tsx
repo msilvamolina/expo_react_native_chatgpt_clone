0d8344b9ffc4e1ed916c1b454392db432c5db8b5
Martín Silva Molina <msilvamolina@gmail.com>
2025-04-02 09:01:22 +0200

    ya tenemos el diseño bottomsheet modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,24 @@
-import { Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { Stack, useRouter } from 'expo-router';
+import { Touchable, TouchableOpacity } from 'react-native';
 
 export default function Layout() {
+  const router = useRouter();
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ presentation: 'modal' }} />
+      <Stack.Screen
+        name="login"
+        options={{
+          presentation: 'modal',
+          title: '',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
     </Stack>
   );
 }
--- app/login.tsx
+++ app/login.tsx
@@ -1,13 +1,108 @@
 import { useLocalSearchParams } from 'expo-router';
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Keyboard,
+  Platform,
+  ActivityIndicator,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+import Colors from '~/constants/Colors';
+import { defaultStyles } from '~/constants/Styles';
 
 const Page = () => {
   const { type } = useLocalSearchParams<{ type: string }>();
+  const [loading, setLoading] = useState(false);
+  const [emailAddress, setEmailAddress] = useState('hola@hola.com');
+  const [password, setPassword] = useState('');
+
+  const onSignUpPress = async () => {};
+  const onSignInPress = async () => {};
+
   return (
-    <View>
-      <Text>{type}</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={70}
+      style={styles.container}>
+      {loading && (
+        <View style={defaultStyles.loadingOverlay}>
+          <ActivityIndicator size="large" color="#fff" />
+        </View>
+      )}
+      <Image style={styles.logo} source={require('../assets/images/logo-dark.png')} />
+      <Text style={styles.title}>{type === 'login' ? 'Welcome back' : 'Create your account'}</Text>
+
+      <View style={{ marginBottom: 30 }}>
+        <TextInput
+          style={styles.inputField}
+          autoCapitalize="none"
+          placeholder="Email"
+          value={emailAddress}
+          onChangeText={setEmailAddress}
+        />
+        <TextInput
+          style={styles.inputField}
+          autoCapitalize="none"
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+
+        {type === 'login' ? (
+          <TouchableOpacity onPress={onSignInPress} style={[defaultStyles.btn, styles.btnPrimary]}>
+            <Text style={styles.btnPrimaryText}>Login</Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={onSignUpPress} style={[defaultStyles.btn, styles.btnPrimary]}>
+            <Text style={styles.btnPrimaryText}>Create account</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    </KeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  logo: {
+    width: 60,
+    height: 60,
+    alignSelf: 'center',
+    marginVertical: 80,
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  inputField: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderColor: Colors.primary,
+    borderRadius: 12,
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  btnPrimary: {
+    backgroundColor: Colors.primary,
+    marginVertical: 10,
+  },
+  btnPrimaryText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
 
 export default Page;

b9738f47278cbef19ea7d6df3352143fb9011fb2
Martín Silva Molina <msilvamolina@gmail.com>
2025-04-03 08:26:43 +0200

    ya tenemos el drawer mostrando items

--- app/(auth)/(drawer)/_layout.tsx
+++ app/(auth)/(drawer)/_layout.tsx
@@ -1,7 +1,12 @@
 // import { FontAwesome6, Ionicons } from '@expo/vector-icons';
 // import { DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 // import { DrawerActions } from '@react-navigation/native';
-import { createDrawerNavigator, DrawerItemList } from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerItemList,
+} from '@react-navigation/drawer';
 import { useNavigation, useRouter } from 'expo-router';
 import { Drawer } from 'expo-router/drawer';
 import { TextInput, Text, Image, View, StyleSheet, useWindowDimensions } from 'react-native';
@@ -11,63 +16,51 @@ import NewChat from './(chat)/new';
 
 import Colors from '~/constants/Colors';
 
-export const CustomDrawerContent = (props: any) => {
-  const { bottom, top } = useSafeAreaInsets();
-
-  return (
-    <View style={{ flex: 1, marginTop: top }}>
-      {/* <View style={{ backgroundColor: '#fff', paddingBottom: 10 }}>
-        <View style={styles.searchSection}>
-          <Ionicons style={styles.searchIcon} name="search" size={20} color={Colors.greyLight} />
-          <TextInput
-            style={styles.input}
-            placeholder="Search"
-            underlineColorAndroid="transparent"
-          />
-        </View>
-      </View> */}
-      {/* <DrawerContentScrollView
-        {...props}
-        contentContainerStyle={{ backgroundColor: '#fff', paddingTop: 0 }}>
-        <DrawerItemList {...props} />
-
-        <DrawerItem
-          label="Nuevo Chat"
-          icon={({ color, size }) => (
-            <Ionicons name="chatbubbles-outline" size={size} color={color} />
-          )}
-          onPress={() => {
-            props.navigation.navigate('new'); // Debe coincidir con name en Drawer.Screen
-          }}
-        />
-      </DrawerContentScrollView> */}
-    </View>
-  );
-};
-
-const Layout = () => {
-  const navigation = useNavigation();
+export function CustomDrawerContent(props: any) {
   const router = useRouter();
-  const dimensions = useWindowDimensions();
 
   return (
-    <Drawer drawerContent={CustomDrawerContent}>
+    <DrawerContentScrollView {...props}>
+      <DrawerItem
+        label="New Chat"
+        icon={({ color, size }) => (
+          <View style={[styles.item, { backgroundColor: '#000' }]}>
+            <Image source={require('~/assets/images/logo-white.png')} style={styles.btnImage} />
+          </View>
+        )}
+        onPress={() => router.push('/(auth)/(drawer)/(chat)/new')}
+      />
+      <DrawerItem label="Explorar" onPress={() => router.push('/(auth)/(drawer)/explore')} />
+    </DrawerContentScrollView>
+  );
+}
+const Layout = () => {
+  return (
+    <Drawer drawerContent={(props) => <CustomDrawerContent {...props} />}>
       <Drawer.Screen
         name="(chat)/new"
+        getId={() => Math.random().toString()}
         options={{
+          drawerLabel: 'Home',
+          drawerItemStyle: { display: 'none' }, // 👈 evita que DrawerItemList lo intente renderizar
+
           title: 'New Chat',
-          drawerIcon: () => (
-            <View>
-              <Text>Hola</Text>
-              {/* <Image source={require('../../../assets/images/logo-white')} /> */}
-            </View>
-          ),
         }}
       />
     </Drawer>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  item: {
+    borderRadius: 15,
+    overflow: 'hidden',
+  },
+  btnImage: {
+    margin: 6,
+    width: 16,
+    height: 16,
+  },
+});
 
 export default Layout;

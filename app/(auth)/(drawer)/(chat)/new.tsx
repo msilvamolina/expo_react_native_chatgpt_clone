81bb9825424292dc88d1944c714bba3ff98aa5d3
Martín Silva Molina <msilvamolina@gmail.com>
2025-04-04 04:36:47 +0200

    tenemos el diseño del boton bar animado

--- app/(auth)/(drawer)/(chat)/new.tsx
+++ app/(auth)/(drawer)/(chat)/new.tsx
@@ -1,9 +1,10 @@
 import { useAuth } from '@clerk/clerk-react';
 import { Stack } from 'expo-router';
 import { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 
 import HeaderDropDown from '~/components/HeaderDropDown';
+import MessageInput from '~/components/MessageInput';
 import { defaultStyles } from '~/constants/Styles';
 
 const Page = () => {
@@ -11,6 +12,7 @@ const Page = () => {
 
   const [gptVersion, setGPTVersion] = useState('3.5');
 
+  const getCompletion = async (message: string) => {};
   return (
     <View style={defaultStyles.pageContainer}>
       <Stack.Screen
@@ -30,7 +32,26 @@ const Page = () => {
           ),
         }}
       />
-      <Button title="Sign Out" onPress={() => signOut()} />
+      <View style={{ flex: 1 }}>
+        <Text>DUMMY CONTENT</Text>
+        <Button title="Sign Out" onPress={() => signOut()} />
+        {/* <ScrollView>
+          {Array.from({ length: 100 }).map((_, index) => (
+            <Text key={index}>{index}</Text>
+          ))}
+        </ScrollView> */}
+      </View>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={70}
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          width: '100%',
+        }}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <MessageInput onShouldSendMessage={getCompletion} />
+      </KeyboardAvoidingView>
     </View>
   );
 };

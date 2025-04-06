329bc8b17a845696b14c5a2fb8d9c397b4827839
Martín Silva Molina <msilvamolina@gmail.com>
2025-04-07 01:25:49 +0200

    tenemos una blur view abajo como bottom bar

--- app/(auth)/(modal)/[url].tsx
+++ app/(auth)/(modal)/[url].tsx
@@ -1,8 +1,13 @@
+import { Ionicons, Octicons } from '@expo/vector-icons';
 import { ImageZoom } from '@likashefqet/react-native-image-zoom';
+import { BlurView } from 'expo-blur';
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import Colors from '~/constants/Colors';
+import { downloadAndSaveImage, shareImage } from '~/utils/Image';
 const Page = () => {
   const { url, prompt } = useLocalSearchParams<{ url: string; prompt?: string }>();
   const { bottom } = useSafeAreaInsets();
@@ -23,6 +28,28 @@ const Page = () => {
         isDoubleTapEnabled
         resizeMode="contain"
       />
+      <BlurView intensity={95} tint="dark" style={[styles.blurview, { paddingBottom: bottom }]}>
+        <View style={styles.row}>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <Ionicons name="chatbubble-ellipses-outline" size={24} color="white" />
+            <Text style={styles.btnText}>Edit</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <Ionicons name="brush-outline" size={24} color="white" />
+            <Text style={styles.btnText}>Select</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ alignItems: 'center' }}
+            onPress={() => downloadAndSaveImage(url)}>
+            <Octicons name="download" size={24} color="white" />
+            <Text style={styles.btnText}>Save</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => shareImage(url)}>
+            <Octicons name="share" size={24} color="white" />
+            <Text style={styles.btnText}>Share</Text>
+          </TouchableOpacity>
+        </View>
+      </BlurView>
     </View>
   );
 };
@@ -34,9 +61,54 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#000',
   },
+  blurview: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 32,
+    paddingVertical: 16,
+  },
   image: {
     width: '100%',
     height: '100%',
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 12,
+    paddingTop: 6,
+  },
+  modalContainer: {
+    flex: 1,
+    paddingHorizontal: 24,
+  },
+  titleText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  promptText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  buttonText: {
+    color: Colors.grey,
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  closeBtn: {
+    backgroundColor: Colors.dark,
+    borderRadius: 20,
+    height: 26,
+    width: 26,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 

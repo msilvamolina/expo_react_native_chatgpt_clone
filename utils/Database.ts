bd52f6f23d6336149d3ba378547085d18c628f10
Martín Silva Molina <msilvamolina@gmail.com>
2025-04-08 02:03:02 +0200

    ya tenemos la base de datos

--- utils/Database.ts
+++ utils/Database.ts
@@ -1,6 +1,8 @@
 import * as FileSystem from 'expo-file-system';
 import { SQLiteDatabase } from 'expo-sqlite';
 
+import { Message, Role } from './interfaces';
+
 export async function migrateDbIfNeeded(db: SQLiteDatabase) {
   // Log DB path for debugging
   console.log(FileSystem.documentDirectory);
@@ -41,3 +43,39 @@ CREATE TABLE messages (
 
   await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`);
 }
+
+export const addChat = async (db: SQLiteDatabase, title: string) => {
+  await db.runAsync('INSERT INTO chats (title) VALUES (?)', title);
+};
+
+export const getChats = async (db: SQLiteDatabase) => {
+  return await db.getAllAsync('SELECT * FROM chats');
+};
+
+export const addMessage = async (
+  db: SQLiteDatabase,
+  chatId: number,
+  { content, role, imageUrl, prompt }: Message
+) => {
+  await db.runAsync(
+    'INSERT INTO messages (chat_id, content, role, imageUrl, prompt) VALUES (?, ?, ?, ?, ?)',
+    chatId,
+    content,
+    role === Role.Bot ? Role.Bot : Role.User,
+    imageUrl || '',
+    prompt || ''
+  );
+};
+
+export const getMessages = async (db: SQLiteDatabase, chatId: number) => {
+  return (await db.getAllAsync<Message>('SELECT * FROM messages WHERE chat_id = ?', chatId)).map(
+    (message) => ({ ...message, role: `${message.role}` === 'bot' ? Role.Bot : Role.User })
+  );
+};
+export const deleteChat = async (db: SQLiteDatabase, chatId: number) => {
+  await db.runAsync('DELETE FROM chats WHERE id = ?', chatId);
+};
+
+export const renameChat = async (db: SQLiteDatabase, chatId: number, title: string) => {
+  return await db.runAsync('UPDATE chats SET title =? WHERE id =?', title, chatId);
+};
